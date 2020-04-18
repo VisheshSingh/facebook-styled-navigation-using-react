d785566a05790f5c01f9dae137ca6bc16d222d94
Vishesh Singh <VisheshSingh16@gmail.com>
2020-04-18 12:14:43 -0500

    menu transitioning WIP ⛑

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { CSSTransition } from 'react-transition-group';
 import { ReactComponent as ArrowIcon } from './icons/arrow.svg';
 import { ReactComponent as BellIcon } from './icons/bell.svg';
 import { ReactComponent as BoltIcon } from './icons/bolt.svg';
@@ -43,9 +44,20 @@ function NavItem(props) {
 }
 
 function DropdownMenu() {
+  const [activeMenu, setActiveMenu] = useState('main'); // settings, animals
+  const [menuHeight, setMenuHeight] = useState(null);
+
+  function calcHeight(el) {
+    const height = el.offsetHeight;
+    setMenuHeight(height);
+  }
   function DropdownItem(props) {
     return (
-      <a href='#' className='menu-item'>
+      <a
+        href='#'
+        className='menu-item'
+        onClick={() => props.gotoMenu && setActiveMenu(props.gotoMenu)}
+      >
         {props.leftIcon && <span className='icon-left'>{props.leftIcon}</span>}
         {props.children}
         {props.rightIcon && (
@@ -55,11 +67,47 @@ function DropdownMenu() {
     );
   }
   return (
-    <div className='dropdown'>
-      <DropdownItem>My Profile</DropdownItem>
-      <DropdownItem leftIcon={<CogIcon />} rightIcon={<ChevronIcon />}>
-        My Settings
-      </DropdownItem>
+    <div className='dropdown' style={{ height: menuHeight }}>
+      <CSSTransition
+        in={activeMenu === 'main'}
+        timeout={500}
+        unmountOnExit
+        classNames='menu-primary'
+        onEnter={calcHeight}
+      >
+        <div className='menu'>
+          <DropdownItem leftIcon={<BoltIcon />}>My Profile</DropdownItem>
+          <DropdownItem
+            leftIcon={<CogIcon />}
+            rightIcon={<ChevronIcon />}
+            gotoMenu='settings'
+          >
+            Settings
+          </DropdownItem>
+        </div>
+      </CSSTransition>
+      <CSSTransition
+        in={activeMenu === 'settings'}
+        timeout={500}
+        unmountOnExit
+        classNames='menu-secondary'
+      >
+        <div className='menu'>
+          <DropdownItem leftIcon={<ArrowIcon />} gotoMenu='main'></DropdownItem>
+          <DropdownItem>Settings</DropdownItem>
+          <DropdownItem>Settings</DropdownItem>
+          <DropdownItem>Settings</DropdownItem>
+          <DropdownItem>Settings</DropdownItem>
+          <DropdownItem>Settings</DropdownItem>
+          <DropdownItem>Settings</DropdownItem>
+          <DropdownItem>Settings</DropdownItem>
+          <DropdownItem>Settings</DropdownItem>
+          <DropdownItem>Settings</DropdownItem>
+          <DropdownItem>Settings</DropdownItem>
+          <DropdownItem>Settings</DropdownItem>
+          <DropdownItem>Settings</DropdownItem>
+        </div>
+      </CSSTransition>
     </div>
   );
 }
