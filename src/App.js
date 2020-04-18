87c0705939d3851947e479f796573ff1d0f94e51
Vishesh Singh <VisheshSingh16@gmail.com>
2020-04-18 10:43:30 -0500

    Top Nav with svgs 🤩🗺

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import { ReactComponent as ArrowIcon } from './icons/arrow.svg';
+import { ReactComponent as BellIcon } from './icons/bell.svg';
+import { ReactComponent as BoltIcon } from './icons/bolt.svg';
+import { ReactComponent as CaretIcon } from './icons/caret.svg';
+import { ReactComponent as ChevronIcon } from './icons/chevron.svg';
+import { ReactComponent as CogIcon } from './icons/cog.svg';
+import { ReactComponent as MessengerIcon } from './icons/messenger.svg';
+import { ReactComponent as PlusIcon } from './icons/plus.svg';
 
 function App() {
-  return <div className='App'>hello</div>;
+  return (
+    <Navbar>
+      <NavItem icon={<PlusIcon />} />
+      <NavItem icon={<BellIcon />} />
+      <NavItem icon={<MessengerIcon />} />
+    </Navbar>
+  );
 }
 
+function Navbar(props) {
+  return (
+    <nav className='navbar'>
+      <ul className='navbar-nav'>{props.children}</ul>
+    </nav>
+  );
+}
+
+function NavItem(props) {
+  return (
+    <li className='nav-item'>
+      <a href='#' className='icon-button'>
+        {props.icon}
+      </a>
+    </li>
+  );
+}
 export default App;
